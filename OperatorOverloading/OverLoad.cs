27464d2e3c94a2fddb4d570ad0eb00c77f680783
Valerii <valeradots81@gmail.com>
2018-09-17 14:42:39 +0300

    2018_09_17

---
+++ OperatorOverloading/OverLoad.cs
@@ -0,0 +1,49 @@
+﻿namespace OperatorOverloading
+{
+    class OverLoad
+    {
+        public int[] Arr = new int[5];
+        public bool check = true;
+        public int count = 0;
+
+        public static OverLoad operator +(OverLoad arr1, OverLoad arr2)
+        {
+            OverLoad arrResult = new OverLoad();
+
+            for (int index = 0; index < 5; index++)
+            {
+                arrResult.Arr[index] = arr1.Arr[index] + arr2.Arr[index];
+            }
+
+            return arrResult;
+        }
+
+        public static bool operator ==(OverLoad arr1, OverLoad arr2)
+        {
+            OverLoad arrResult = new OverLoad();
+
+            for (int index = 0; index < arrResult.Arr.Length; index++)
+            {
+                if (arr1.Arr[index] != arr2.Arr[index]) arrResult.check = false;
+            }
+            return arrResult.check;
+        }
+
+        public static bool operator !=(OverLoad arr1, OverLoad arr2)
+        {
+            OverLoad arrResult = new OverLoad();
+
+            for (int index = 0; index < arrResult.Arr.Length; index++)
+            {
+                if (arr1.Arr[index] == arr2.Arr[index]) arrResult.count++;
+            }
+
+            if (arrResult.count == arrResult.Arr.Length)
+            {
+                arrResult.check = false;
+            }
+
+            return arrResult.check;
+        }
+    }
+}
---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+//Create a class in which to restart the operators: "+", "==", "! =", For arrays of the form int[] Arr = new int[5];
+//"+" - array elements are indexed sequentially;
+//"==" - true if array elements are identical;
+//"! =" - true, if there is a difference, although b is in one element.
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OverLoad arr1 = new OverLoad();
+            OverLoad arr2 = new OverLoad();
+            OverLoad arr3 = new OverLoad();
+
+            arr1.Arr = new int[] { 0, 2, 5, 15, 3 };
+            arr2.Arr = new int[] { 0, 2, 5, 15, 3 };
+
+            arr3 = arr1 + arr2;
+
+            Console.WriteLine(arr1 == arr2);
+
+            Console.WriteLine(arr1 != arr2);
+
+            foreach (int number in arr3.Arr)
+            {
+                Console.Write("{0} ", number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
